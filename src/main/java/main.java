68410dc85923fbfec018930390137455bee33331
GrantIee <grant.lee.2017@gmail.com>
2019-05-20 14:48:50 -0400

    initialization

---
+++ src/main/java/main.java
@@ -0,0 +1,203 @@
+public class main {
+    String unfiltered_html = "<html><head><title>Strawberry Pop-Tart Blow-Torches</title></head>\n" +
+            "\n" +
+            "<body>\n" +
+            "<h1>\t<img src=\"ptart-1c.gif\" alt=\"\" align=\"middle\">\n" +
+            "\tStrawberry Pop-Tart Blow-Torches\n" +
+            "</h1>\n" +
+            "\n" +
+            "<em>Last Updated: 2G August 1994</em>\n" +
+            "\n" +
+            "<h2>Author</h2>\n" +
+            "\t<a href=\"http://www.pmichaud.com/\">\n" +
+            "\t<address>Patrick R. Michaud, pmichaud@pobox.com</address>\n" +
+            "\t</a>\n" +
+            "\n" +
+            "<h2>Abstract</h2>\n" +
+            "\tStrawberry Pop Tarts may be a cheap and inexpensive source of\n" +
+            "\tincendiary devices.  Toasters which fail to eject Pop Tarts\n" +
+            "\tcause the Pop Tarts to emit flames 10-18 inches in height.\n" +
+            "\n" +
+            "<h2>Introduction</h2>\n" +
+            "\tLast year, an article by well-known newspaper columnist\n" +
+            "\t<em>Dave Barry</em> noted that <em>Kellogg's Strawberry\n" +
+            "\tPop Tarts</em> (SPTs) could be made to emit flames \"like a \n" +
+            "\tblow torch\" if left in a toaster too long.  Given previous \n" +
+            "\twork in the field of food-entertainment (see \n" +
+            "\t<a href=\"http://www.pmichaud.com/grape/\">\n" +
+            "\t<em>Fun With Grapes - A Case Study</em></a>), it was obvious\n" +
+            "\tthat this was a new frontier that requires further exploration.  \n" +
+            "\tThe present work describes our independent verification and \n" +
+            "\texperience with SPT-based combustion.\n" +
+            "\n" +
+            "<h2>Materials Used</h2>\n" +
+            "\tOnly two basic materials are needed to cause SPT-combustion:\n" +
+            "\ta (hopefully inexpensive) toaster and some Strawberry Pop Tarts\n" +
+            "\t(Figure 1).\n" +
+            "\tIn this work, the authors used <em>Kellogg's Strawberry Pop Tarts\n" +
+            "\twith Real Smucker's Fruit</em>.  SPTs can be obtained either with\n" +
+            "\tor without frosting; the non-frosted variety were used for this\n" +
+            "\texperiment.\n" +
+            "    <p>\n" +
+            "\t<a href=\"toast-1a.gif\">\n" +
+            "\t<img src=\"toast-1b.gif\" alt=\"Figure 1\"><br>\n" +
+            "\t<strong>Figure 1. Toaster and Strawberry Pop Tarts</strong></a>\n" +
+            "    </p><p>\n" +
+            "\tIn addition to the basic materials, a number of safety-related\n" +
+            "\titems were needed to conduct this experiment.  First, a suitable\n" +
+            "\tlocation for the experiment was required, it being expected that\n" +
+            "\tthe kitchen was not the appropriate place for blow-torching SPTs.\n" +
+            "\tThe author's driveway was chosen as a suitable site.  Second,\n" +
+            "\tan appropriate means for extinguishing the SPTs would be needed;\n" +
+            "\ta research assistant brought along some baking soda for the\n" +
+            "\tpurpose.\n" +
+            "\n" +
+            "</p><h2>Experiment Preparation</h2>\n" +
+            "\tThe toaster and SPT both had to be properly prepared for this\n" +
+            "\texperiment.  In order to guarantee that the SPT would receive\n" +
+            "\tsufficient heat to begin combustion, the toaster was set to\n" +
+            "\tits highest setting and the lever was jammed in the \"down\"\n" +
+            "\tposition using adhesive cellophane.  A SPT was removed from\n" +
+            "\tthe box and its protective packaging and carefully placed into\n" +
+            "\tthe toaster slot (Figure 2).\n" +
+            "    <p>\n" +
+            "\t<a href=\"toast-2a.gif\">\n" +
+            "\t<img src=\"toast-2c.gif\" alt=\"Figure 2\"><br>\n" +
+            "\t<strong>Figure 2. Preparation of Toaster and SPT</strong></a>\n" +
+            "    </p><p>\n" +
+            "\tNext, the toaster and SPT were taken to the driveway, and an\n" +
+            "\textension cord was arranged to provide power to the toaster.\n" +
+            "\tAt this point, we were ready to begin the experiment.\n" +
+            "    </p><p> \n" +
+            "\t<a href=\"toast-3a.gif\">\n" +
+            "\t<img src=\"toast-3c.gif\" alt=\"Figure 3\"><br>\n" +
+            "\t<strong>Figure 3. Toaster Prepared for SPT combustion</strong></a>\n" +
+            "\n" +
+            "</p><h2>The Experiment and Observations</h2>\n" +
+            "\tThe toaster was plugged in.  First the toaster went through a\n" +
+            "\tnormal \"toasting\" cycle (approximately 60 seconds), \n" +
+            "\twhich more than thoroughly cooked the SPT (since the toaster \n" +
+            "\twas set to its darkest setting).  By this point we could\n" +
+            "\tdefinitely detect a burnt SPT aroma.\n" +
+            "\tThe toaster then attempted to eject the SPT, but was prevented from\n" +
+            "\tdoing so by the adhesive cellophane.  The toaster then began\n" +
+            "\temitting loud rattling and buzzing noises due to its inability\n" +
+            "\tto eject the SPT.\n" +
+            "    <p>\n" +
+            "\t(At this point the researchers became somewhat concerned that\n" +
+            "\tthe noise from the toaster would wake the neighbors and\n" +
+            "\tattract undue attention.  However, we decided that we were\n" +
+            "\talready committed to the experiment and that the neighbors\n" +
+            "\twould be able to sacrifice some sleep in the name of science.)\n" +
+            "    </p><p>\n" +
+            "\tSoon thereafter, large amounts of smoke began pouring out of\n" +
+            "\tthe toaster.  The researchers noticed that some of the neighbors\n" +
+            "\tdown the street were beginning to get a little curious, but\n" +
+            "\tthe experiment proceeded nonetheless.  Approximately 40 seconds\n" +
+            "\tlater, small flames began licking their way out of the toaster.\n" +
+            "\tThe flames steadily grew larger and larger until reaching a\n" +
+            "\tmaximum height of about 18 inches above the top of the toaster.\n" +
+            "\tFigure 4 presents a time-series collage of the flames emitted\n" +
+            "\tfrom the SPT.\n" +
+            "    </p><p>\n" +
+            "\t<a href=\"toast-4a.gif\">\n" +
+            "\t<img src=\"toast-4c.gif\" alt=\"Figure 4\"><br>\n" +
+            "\t<strong>Figure 4.  Time Series Photograph of Flaming SPT</strong></a>\n" +
+            "    </p><p>\n" +
+            "\tAs the flames were reaching their maximum height, the toaster\n" +
+            "\tabruptly stopped making buzzing noises.  We speculate that the\n" +
+            "\tflames had by this point shorted the electronics within the\n" +
+            "\ttoaster.  The toaster was quickly disconnected from the primary\n" +
+            "\telectrical source to avoid any potential damage to the author's\n" +
+            "\thouse.  At this point, the researchers also realized that the\n" +
+            "\theat could inadvertently melt the adhesive cellophane and\n" +
+            "\tcause the flaming SPTs to suddenly eject from the toaster.\n" +
+            "\tUnfortunately, this did not occur.  The flames continued \n" +
+            "\tfor several minutes.\n" +
+            "    </p><p>\n" +
+            "\tAt this point there was some slight concern that the flames\n" +
+            "\tmight take considerable time to diminish.  We then enlisted the\n" +
+            "\thelp of a reluctant research assistant to sprinkle baking soda\n" +
+            "\ton the flames.  (The reluctance was understandable given the\n" +
+            "\tpotential for premature SPT ejection described in the above\n" +
+            "\tparagraph.)  The baking soda quickly extinguished the flames\n" +
+            "\tand produced still further smoke (Figure 5a).\n" +
+            "    </p><p> \n" +
+            "\t<a href=\"toast-5a.gif\">\n" +
+            "\t<img src=\"toast-5c.gif\" alt=\"Figure 5\"><br>\n" +
+            "\t<strong>Figure 5. Extinguising the SPT</strong></a>\n" +
+            "    </p><p>\n" +
+            "\tOnce the flames were extinguished, the researchers noted an\n" +
+            "\tunanticipated problem: what to do with the (now defunct)\n" +
+            "\ttoaster and the spent SPT.  It became obvious that the toaster\n" +
+            "\tcould not be returned to the author's house due to both a \n" +
+            "\tcontinued potential fire hazard and the smell of burnt\n" +
+            "\tstrawberries.  In addition, it was noted that the toaster was\n" +
+            "\tstill \"too hot to handle,\" necessitating the use of a nearby\n" +
+            "\tgarden hose to cool the toaster off.  This is illustrated\n" +
+            "\tin Figure 5b.  Finally it was decided to just leave the toaster\n" +
+            "\tby the curb for the sanitation experts to pick up the next\n" +
+            "\tmorning (Figure 6.)\n" +
+            "    </p><p>\n" +
+            "\t<a href=\"toast-6a.html\">\n" +
+            "\t<img src=\"toast-6c.gif\" alt=\"Figure 6\"><br>\n" +
+            "\t<strong>Figure 6. Toaster Disposal</strong></a>\n" +
+            "    </p><p>\n" +
+            "\n" +
+            "</p><h2>Summary and Recommendations</h2>\n" +
+            "\tIn summary, overcooking the SPT did produce a good size flame.\n" +
+            "\tThe effect was not as pronounced as the researchers had hoped, but\n" +
+            "\twas satisfying nonetheless.  The research assistant noted that\n" +
+            "\tthe flames produced did appear to have some color variation.\n" +
+            "\tWe believe that frosted SPTs may successfully produce even\n" +
+            "\tlarger torches.  Further research in this area is warranted.\n" +
+            "    <p>\n" +
+            "\tWe did desire to repeat the experiment with the remaining five\n" +
+            "\tSPTs, but we could not do so because there were no more suitable\n" +
+            "\ttoasters available for further experiments.  In the future, we recommend\n" +
+            "\tthat toasters be sold in six-packs to accomodate important\n" +
+            "\tSPT research.  Instead, the remaining SPTs were sacrified over\n" +
+            "\tthe course of the next several days in private, undocumented\n" +
+            "\tconsumption experiments.\n" +
+            "\n" +
+            "</p><h2>Acknowledgements</h2>\n" +
+            "\tSpecial thanks to\n" +
+            "\tJennifer \"Svetlana\" Reckard for her suggestions and\n" +
+            "\tproofreading of this work.\n" +
+            "    <p>\n" +
+            "</p><hr>\n" +
+            "<h2>Followup Comments</h2>\n" +
+            "   <p>\tThe response to my Strawberry Pop Tart article has been overwhelming.\n" +
+            "\tIn October 1994 alone it's been accessed more than 2000 times.  Here\n" +
+            "\tare some of the terrific followup messages I've received:\n" +
+            "   </p><ol>\n" +
+            "   <li>\t<a href=\"follow1.html\">\n" +
+            "\t12-Aug-94: <tt>schnitz@unipress.com</tt>\n" +
+            "\t</a>\n" +
+            "   </li><li>\t<a href=\"follow2.html\">\n" +
+            "\t13-Oct-94: <tt>avonruff@hydra.urbana.mcd.mot.com</tt>\n" +
+            "\t</a>\n" +
+            "   \n" +
+            "\tPlease send me your comments!  I'm especially interested to learn\n" +
+            "\thow you found out about this page, since I haven't advertised it\n" +
+            "\tmuch of anywhere.\n" +
+            "<hr>\n" +
+            "   <address>\n" +
+            "        <img src=\"krnbo24.gif\" alt=\"\" align=\"top\">\n" +
+            "        <a href=\"http://www.pmichaud.com/\">\n" +
+            "        Patrick R. Michaud/pmichaud@pobox.com\n" +
+            "        </a>\n" +
+            "   <address>\n" +
+            "<script src=\"http://www.google-analytics.com/urchin.js\" type=\"text/javascript\">\n" +
+            "</script>\n" +
+            "<script type=\"text/javascript\">\n" +
+            "_uacct = \"UA-582887-2\";\n" +
+            "urchinTracker();\n" +
+            "</script>\n" +
+            "\n" +
+            "\n" +
+            "</address></address></li></ol></body></html>";
+
+    String filtered_html
+
+}
